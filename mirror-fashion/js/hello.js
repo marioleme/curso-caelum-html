b5565a2c4c2a68e8f2569e86fb5c0e607c284802
Mario Oliveira <mario.oliveirag12@gmail.com>
2015-05-21 21:03:52 -0300

    exercicios
    exercicios  caelum Javascrip sbanner

--- mirror-fashion/js/hello.js
+++ mirror-fashion/js/hello.js
@@ -16,10 +16,29 @@ document.querySelector('#busca').onfocus = function() {
 	 return false ; 
 		
 };
-	
-	function trocaBanner (){
-	 document.querySelector('#banner').src="verao"
-	};
-	
-	var timer = setInterval(minhaFuncao,2000);
-	
+ 
+var banners = ["img/destaque-home.png","img/destaque-home-2.png"];
+var bannerAtual = 0;
+  function trocaBanner()
+  {
+  	 bannerAtual = (bannerAtual + 1) % 2;
+  	 document.querySelector('.destaque img').src = banners[bannerAtual];
+  }
+  var timer = setInterval (trocaBanner , 4000);
+
+
+ var controle = document.querySelector('.pause');
+ controle.onclick = function()
+ {
+ 	if(controle.className =='pause')
+ 	{ 
+ 		clearInterval(timer);
+
+ 	}else{
+ 		timer = setInterval(trocaBanner, 4000);
+ 		controle.className = 'pause';
+ 	}
+
+ 	return false;
+ };
+
